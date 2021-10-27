481e9badc4b97ec5c6c83d6df747b2b154afd4cf
BarrieLAJ <alhajiab.barrie@gmail.com>
2021-10-27 12:28:29 +0000

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'antd/dist/antd.css';
+
+// import {ConfigProvider} from 'antd'
+
+// ConfigProvider.config({
+//   theme: {
+//     primaryColor: 
+//   }
+// })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
